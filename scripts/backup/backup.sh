f6afba68cca72e7e5ef5ea597023c069a7b39a75
RyoSchano <ryoue.schano@gmail.com>
2026-07-16 14:38:27 +0900

    Fix and update backup.sh

--- scripts/backup/backup.sh
+++ scripts/backup/backup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 SOURCE="$1"
-ARCHIVE_DIR="$HOME/archives/$SOURCE_$(date '+%Y-%m-%d_%H-%M-%S').tar.gz"
+ARCHIVE_DIR="$HOME/archives"
+ARCHIVE_FILE="$ARCHIVE_DIR/$SOURCE_$(date '+%Y-%m-%d_%H-%M-%S').tar.gz"
 

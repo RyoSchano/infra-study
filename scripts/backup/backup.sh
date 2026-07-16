fc04f6685bdb38100edfc0cf80d74e5a8fe5fd19
RyoSchano <ryoue.schano@gmail.com>
2026-07-16 15:03:51 +0900

    Fix backup.sh

--- scripts/backup/backup.sh
+++ scripts/backup/backup.sh
@@ -5,7 +5,8 @@ SOURCE_NAME=$(basename "$SOURCE")
 DATE=$(date '+%Y-%m-%d')
 TIME=$(date '+%H-%M-%S')
 ARCHIVE_DIR="$HOME/archives"
-ARCHIVE_FILE="$ARCHIVE_DIR/$SOURCE_NAME_$DATE_$TIME.tar.gz"
+ARCHIVE_FILE="${ARCHIVE_DIR}/${SOURCE_NAME}_${DATE}_${TIME}.tar.gz"
 LOG_DIR="$HOME/backup-logs"
-LOG_FILE="$LOG_DIR/${DATE}-backup.log"
+LOG_FILE="$LOG_DIR/${DATE}-.log"
+
 
